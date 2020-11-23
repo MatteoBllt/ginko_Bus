dac41e662a28d4a4f2c953b9012623617ad2bdac
Arcadia-Matteo <matteo25310@gmail.com>
2020-11-23 11:43:52 +0100

    manifest5

--- app.js
+++ app.js
@@ -7,7 +7,41 @@
  ************************************************************************/
 document.addEventListener("DOMContentLoaded", function (_e) {
 
-    
+    if('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./pwa-examples/js13kpwa/sw.js');
+    };
+    self.addEventListener('install', (e) => {
+        console.log('[Service Worker] Install');
+    });
+
+    self.addEventListener('install', (e) => {
+        console.log('[Service Worker] Install');
+        e.waitUntil(
+          caches.open(cacheName).then((cache) => {
+                console.log('[Service Worker] Caching all: app shell and content');
+            return cache.addAll(contentToCache);
+          })
+        );
+      });
+
+    self.addEventListener('fetch', (e) => {
+        console.log('[Service Worker] Fetched resource '+e.request.url);
+    });
+
+    self.addEventListener('fetch', (e) => {
+        e.respondWith(
+          caches.match(e.request).then((r) => {
+                console.log('[Service Worker] Fetching resource: '+e.request.url);
+            return r || fetch(e.request).then((response) => {
+                      return caches.open(cacheName).then((cache) => {
+                console.log('[Service Worker] Caching new resource: '+e.request.url);
+                cache.put(e.request, response.clone());
+                return response;
+              });
+            });
+          })
+        );
+      });
     /******************************************************************
             Fonctions à compléter dans la dernière partie du TP 
     ******************************************************************/
